d4e323a638913761769584fa5df255edeef21ce9
ShikharArvind <shikhararvind@tutanota.com>
2021-09-27 09:55:45 +0200

    Fix rect function comment

--- src/apertures.rs
+++ src/apertures.rs
@@ -1,6 +1,6 @@
 use ndrustfft::*;
 
-// Implement simple fftshift for 1d using slices and concatenation
+// Simple rect function
 pub fn rect_1d(x: f64) -> Complex<f64> {
     if x.abs() <= 0.5 {
         Complex::new(1.0, 0.0)
