d30308f05e1cb69b2fccce9a4d23fa60cccceee3
ShikharArvind <shikhararvind@tutanota.com>
2021-09-27 10:40:40 +0200

    Convert the fftshift functions parameter to types to generic with the necessity of clone trait only (needed for slice function)

--- src/fftshift.rs
+++ src/fftshift.rs
@@ -1,9 +1,8 @@
 use ndarray::concatenate;
 use ndarray::prelude::*;
-use ndrustfft::*;
 
 // Implement simple fftshift for 1d using slices and concatenation
-pub fn fftshift1d(x: &Array1<Complex<f64>>) -> Array1<Complex<f64>> {
+pub fn fftshift1d<T: std::clone::Clone>(x: &Array1<T>) -> Array1<T> {
     let n = (*x).len_of(Axis(0)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
     let n_div_2 = (f64::from(n) / 2.0).ceil() as i32; // Ceil of n/2 works for both odd and even n
     concatenate![
@@ -14,7 +13,7 @@ pub fn fftshift1d(x: &Array1<Complex<f64>>) -> Array1<Complex<f64>> {
 }
 
 // Implement simple ifftshift for 1d using slices and concatenation
-pub fn ifftshift1d(x: &Array1<Complex<f64>>) -> Array1<Complex<f64>> {
+pub fn ifftshift1d<T: std::clone::Clone>(x: &Array1<T>) -> Array1<T> {
     let n = (*x).len_of(Axis(0)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
     let n_div_2 = (f64::from(n) / 2.0).floor() as i32; // Ceil of n/2 works for both odd and even n
     concatenate![
@@ -24,10 +23,9 @@ pub fn ifftshift1d(x: &Array1<Complex<f64>>) -> Array1<Complex<f64>> {
     ]
 }
 
-// Implement simple fftshift for 2d using slices and concatenation. This is very much an extension
-// of 1D swapping. Honestly a common fftshift and iffshit for 1d->3d can be implemented like numpy
-// Implement a generic type i.e. Complex<f64> or <f64> (TODO)
-pub fn fftshift2d(x: &Array2<f64>) -> Array2<f64> {
+// Implement simple fftshift for 2d using slices and concatenation.
+// A common fftshift and iffshit for 1d->3d can be implemented like numpy (TODO)
+pub fn fftshift2d<T: std::clone::Clone>(x: &Array2<T>) -> Array2<T> {
     let n = (*x).len_of(Axis(0)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
     let n_div_2 = (f64::from(n) / 2.0).ceil() as i32; // Ceil of n/2 works for both odd and even n
     let concat_ = concatenate![
@@ -45,7 +43,7 @@ pub fn fftshift2d(x: &Array2<f64>) -> Array2<f64> {
 }
 
 // Implement simple ifftshift for 2d using slices and concatenation.
-pub fn ifftshift2d(x: &Array2<f64>) -> Array2<f64> {
+pub fn ifftshift2d<T: std::clone::Clone>(x: &Array2<T>) -> Array2<T> {
     let n = (*x).len_of(Axis(0)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
     let n_div_2 = (f64::from(n) / 2.0).floor() as i32; // Floor of n/2 works for both odd and even n
     let concat_ = concatenate![
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 // Learning Rust and Fourier Optics :)
-// This is basic implementation of a 1D rectangular function with its Fourier Transform
-// Refer Computational Fourier Optics by Davis Volez - Chap 3, section 3.2
+// Refer Computational Fourier Optics by Davis Volez - Chap 3
 use ndarray::prelude::*;
 use ndrustfft::*;
 mod apertures;
 mod fftshift;
 fn main() {
+    // Simple rectangular 1d function and it Fourier transform
     let w: f64 = 0.055;
     let l: f64 = 2.0;
     let m: f64 = 201.0;
@@ -19,24 +19,24 @@ fn main() {
     fft_out = fftshift::ifftshift1d(&fft_out);
     println!("{} ", fft_out);
 
-    //Test code for the fftshift2d
-    // let array2d_odd = arr2(&[
-    //     [0.0, 1.0, 2.0, 3.0, 4.0],
-    //     [5.0, 6.0, 7.0, 8.0, 9.0],
-    //     [10.0, 11.0, 12.0, 13.0, 14.0],
-    //     [15.0, 16.0, 17.0, 18.0, 19.0],
-    //     [20.0, 21.0, 22.0, 23.0, 24.0],
-    // ]);
-    // let array2d_eve = arr2(&[
-    //     [0.0, 1.0, 2.0, 3.0],
-    //     [5.0, 6.0, 7.0, 8.0],
-    //     [10.0, 11.0, 12.0, 13.0],
-    //     [15.0, 16.0, 17.0, 18.0],
-    // ]);
-    // let eve_fftshift2d = fftshift2d(&array2d_eve);
-    // let odd_fftshift2d = fftshift2d(&array2d_odd);
-    // println!("{}", eve_fftshift2d);
-    // println!("{}", ifftshift2d(&eve_fftshift2d));
-    // println!("{}", odd_fftshift2d);
-    // println!("{}", ifftshift2d(&odd_fftshift2d));
+    //Test code odd odd and even size square arrays for the fftshift in 2d
+    let array2d_odd = arr2(&[
+        [0.0, 1.0, 2.0, 3.0, 4.0],
+        [5.0, 6.0, 7.0, 8.0, 9.0],
+        [10.0, 11.0, 12.0, 13.0, 14.0],
+        [15.0, 16.0, 17.0, 18.0, 19.0],
+        [20.0, 21.0, 22.0, 23.0, 24.0],
+    ]);
+    let array2d_eve = arr2(&[
+        [0.0, 1.0, 2.0, 3.0],
+        [5.0, 6.0, 7.0, 8.0],
+        [10.0, 11.0, 12.0, 13.0],
+        [15.0, 16.0, 17.0, 18.0],
+    ]);
+    let eve_fftshift2d = fftshift::fftshift2d(&array2d_eve);
+    let odd_fftshift2d = fftshift::fftshift2d(&array2d_odd);
+    println!("{}", eve_fftshift2d);
+    println!("{}", fftshift::ifftshift2d(&eve_fftshift2d));
+    println!("{}", odd_fftshift2d);
+    println!("{}", fftshift::ifftshift2d(&odd_fftshift2d));
 }
