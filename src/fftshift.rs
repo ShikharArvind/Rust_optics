85ff09f4d42897fd4ea72121af187672e4a92694
ShikharArvind <shikhararvind@tutanota.com>
2021-09-27 17:48:20 +0200

    Added general fftshift(x) and iffshift(x) function where x is an array of n dimensions

--- src/fftshift.rs
+++ src/fftshift.rs
@@ -1,5 +1,6 @@
 use ndarray::concatenate;
 use ndarray::prelude::*;
+use ndarray::Slice;
 
 // Implement simple fftshift for 1d using slices and concatenation
 pub fn fftshift1d<T: std::clone::Clone>(x: &Array1<T>) -> Array1<T> {
@@ -23,7 +24,8 @@ pub fn ifftshift1d<T: std::clone::Clone>(x: &Array1<T>) -> Array1<T> {
     ]
 }
 
-// Implement simple fftshift for 2d using slices and concatenation.
+// Implement simple fftshift for 2d using slices and concatenation. This is very much an extension
+// of 1D swapping.
 // A common fftshift and iffshit for 1d->3d can be implemented like numpy (TODO)
 pub fn fftshift2d<T: std::clone::Clone>(x: &Array2<T>) -> Array2<T> {
     let n = (*x).len_of(Axis(0)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
@@ -58,4 +60,38 @@ pub fn ifftshift2d<T: std::clone::Clone>(x: &Array2<T>) -> Array2<T> {
         concat_.slice(s![.., m_div_2..m]),
         concat_.slice(s![.., 0..m_div_2])
     ]
+}
+
+//General FFTSHIFT function for n dimensions, required ArrayD hence array_input.into_dyn() necessary
+pub fn fftshiftgen<T: std::clone::Clone>(x: &ArrayD<T>) -> ArrayD<T> {
+    let mut concat_ = (*x).clone(); //Is this the best way to do it - Cloning ?
+    for i in 0..concat_.ndim() {
+        let n = concat_.len_of(Axis(i)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
+        let n_div_2 = (f64::from(n) / 2.0).ceil() as i32; // Ceil of n/2 works for both odd and even n
+        let slice_1 = Slice::from(n_div_2..n);
+        let slice_2 = Slice::from(0..n_div_2);
+        concat_ = concatenate![
+            Axis(i),
+            concat_.slice_axis(Axis(i), slice_1),
+            concat_.slice_axis(Axis(i), slice_2)
+        ]
+    }
+    return concat_;
+}
+
+//General IFFTSHIFT function for n dimensions, required ArrayD hence &(array_input.into_dyn()) necessary
+pub fn ifftshiftgen<T: std::clone::Clone>(x: &ArrayD<T>) -> ArrayD<T> {
+    let mut concat_ = (*x).clone(); //Is this the best way to do it - Cloning ?
+    for i in 0..concat_.ndim() {
+        let n = concat_.len_of(Axis(i)) as i32; // Casting usize (u64) to i32 , array sizes are limited to isize::MAX anyway (?)
+        let n_div_2 = (f64::from(n) / 2.0).floor() as i32; // Ceil of n/2 works for both odd and even n
+        let slice_1 = Slice::from(n_div_2..n);
+        let slice_2 = Slice::from(0..n_div_2);
+        concat_ = concatenate![
+            Axis(i),
+            concat_.slice_axis(Axis(i), slice_1),
+            concat_.slice_axis(Axis(i), slice_2)
+        ]
+    }
+    return concat_;
 }
--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,18 @@ use ndrustfft::*;
 mod apertures;
 mod fftshift;
 fn main() {
-    // Simple rectangular 1d function and it Fourier transform
+    //Simple rectangular 1d function and it Fourier transform
     let w: f64 = 0.055;
     let l: f64 = 2.0;
     let m: f64 = 201.0;
     let dx: f64 = l / m;
     let x_range = Array::range(-l / 2.0, l / 2.0, dx);
     let array_input = x_range.mapv(|a| apertures::rect_1d(a / (2.0 * w)));
-    let fftshift_array_input = fftshift::fftshift1d(&array_input);
+    let fftshift_array_input = fftshift::fftshiftgen(&array_input.into_dyn());
     let mut handler: FftHandler<f64> = FftHandler::new(fftshift_array_input.len());
-    let mut fft_out = Array1::<Complex<f64>>::zeros(fftshift_array_input.len());
+    let mut fft_out = Array1::<Complex<f64>>::zeros(fftshift_array_input.len()).into_dyn();
     ndfft_par(&fftshift_array_input, &mut fft_out, &mut handler, 0);
-    fft_out = fftshift::ifftshift1d(&fft_out);
+    fft_out = fftshift::ifftshiftgen(&fft_out);
     println!("{} ", fft_out);
 
     //Test code odd odd and even size square arrays for the fftshift in 2d
@@ -33,10 +33,7 @@ fn main() {
         [10.0, 11.0, 12.0, 13.0],
         [15.0, 16.0, 17.0, 18.0],
     ]);
-    let eve_fftshift2d = fftshift::fftshift2d(&array2d_eve);
-    let odd_fftshift2d = fftshift::fftshift2d(&array2d_odd);
-    println!("{}", eve_fftshift2d);
-    println!("{}", fftshift::ifftshift2d(&eve_fftshift2d));
-    println!("{}", odd_fftshift2d);
-    println!("{}", fftshift::ifftshift2d(&odd_fftshift2d));
+    let fft2d_test = fftshift::fftshiftgen(&array2d_odd.into_dyn());
+    println!("{}", fft2d_test);
+    println!("{}", fftshift::ifftshiftgen(&fft2d_test));
 }
