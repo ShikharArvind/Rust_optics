9fd24085eafcdce569b68f8adc45bb46eccbb414
ShikharArvind <shikhararvind@tutanota.com>
2021-09-30 12:23:46 +0200

    Replace <Complex<f64>> type with shorter <Complex64> from num_complex crate

--- src/apertures.rs
+++ src/apertures.rs
@@ -1,10 +1,10 @@
-use ndrustfft::*;
+use num_complex::Complex64;
 
 // Simple rect function
-pub fn rect_1d(x: f64) -> Complex<f64> {
+pub fn rect_1d(x: f64) -> Complex64 {
     if x.abs() <= 0.5 {
-        Complex::new(1.0, 0.0)
+        Complex64::new(1.0, 0.0)
     } else {
-        Complex::new(0.0, 0.0)
+        Complex64::new(0.0, 0.0)
     }
 }
