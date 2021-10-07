923688e0a2eb2d2864f6db6d787960f9a44f8ce4
ShikharArvind <shikhararvind@tutanota.com>
2021-10-07 17:19:55 +0200

    Add comments to writer function + TODO

--- src/writer.rs
+++ src/writer.rs
@@ -6,17 +6,22 @@ pub fn write_to_file<T: std::fmt::Display>(file_name: &String, input: &Array2<T>
 
     write!(f, "[").expect("Writing failed");
     for i in 0..(*input).len_of(Axis(0)) {
+        // write the ith row with out comma
         write!(f, "[").expect("Writing failed");
         for iter in (*input).index_axis(Axis(1), i).indexed_iter() {
             if iter.0 == (*input).len_of(Axis(1)) - 1 {
+                // write the last element of the row with out comma
                 write!(f, "{}", *(iter.1)).expect("Writing failed");
             } else {
+                // write the all elements of the row except last separated by comma
                 write!(f, "{},", *(iter.1)).expect("Writing failed");
             }
         }
         if i == (*input).len_of(Axis(0)) - 1 {
+            // close the ith row with ] and no line break
             write!(f, "]").expect("Writing failed");
         } else {
+            // close the last row with ] and line break (this is wrong TODO)
             writeln!(f, "]").expect("Writing failed");
         }
     }
